d1ee32f90c28e4b928af0055df2996e73423eff3
mirovsky <mirovsky@9b7822f3-4786-4c31-9c96-ea234496b5ad>
2024-08-22 10:47:47 +0000

    Updated the ponk version
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/ponk/projects/ponk@260 9b7822f3-4786-4c31-9c96-ea234496b5ad

--- system/ponk.pl
+++ system/ponk.pl
@@ -26,7 +26,7 @@ binmode STDERR, ':encoding(UTF-8)';
 
 my $start_time = [gettimeofday];
 
-my $VER = '0.14 20240821'; # version of the program
+my $VER = '0.15 20240822'; # version of the program
 
 my @features = ('testink ponk-app1');
 
