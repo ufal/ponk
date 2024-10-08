5725963151c7126ec475abe6584be041ea7db482
mirovsky <mirovsky@9b7822f3-4786-4c31-9c96-ea234496b5ad>
2024-10-08 12:26:39 +0000

    First step in migrating from bootstrap version 3 to version 5
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/ponk/projects/ponk@302 9b7822f3-4786-4c31-9c96-ea234496b5ad

--- html/header.php
+++ html/header.php
@@ -33,47 +33,60 @@ $currentLang = $_SESSION['lang'];
 <head>
   <title>PONK</title>
   <meta charset="utf-8">
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"> <!-- Aktualizovaná verze Font Awesome -->
+  
   <link rel="stylesheet" href="css/lindat.css" type="text/css" />
   <link rel="stylesheet" href="css/ponk.css" type="text/css" />
 
-  <script src="https://code.jquery.com/jquery-1.11.3.min.js" type="text/javascript"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js" type="text/javascript"></script>
+  <!-- Bootstrap 5 JavaScript, nyní bez jQuery -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- run_prettify.js není součástí Bootstrapu, takže jej ponechte, pokud ho stále potřebujete -->
 </head>
 
 <body id="lindat-services">
   <!--?php require('branding/header.htm')?-->
   <div class="lindat-common">
     <div class="container">
-      <!--ul class="breadcrumb">
-        <li><a href="//lindat.mff.cuni.cz/">LINDAT/CLARIN</a></li>
-        <li><a href="//lindat.mff.cuni.cz/services/">Services</a></li>
-        <li class="active">MasKIT</li>
-      </ul-->
 
       <!-- Service title -->
       <h1 class="text-center">PONK</h1>
 
       <!-- menu -->
-      <div class="menu-container" style="position: relative;"> <!-- kontejner pro umístění vlaječek vpravo -->
-        <ul class="nav nav-tabs text-center" style="margin-bottom: 10px">
-          <li <?php if ($main_page == 'info.php') echo ' class="active"'?>><a href="info.php"><span class="fa fa-info-circle"></span> <?php echo $lang[$currentLang]['menu_about']; ?></a></li>
-          <li <?php if ($main_page == 'run.php') echo ' class="active"'?>><a href="run.php"><span class="fa fa-cogs"></span> <?php echo $lang[$currentLang]['menu_run']; ?></a></li>
-          <li <?php if ($main_page == 'api-reference.php') echo ' class="active"'?>><a href="api-reference.php"><span class="fa fa-list"></span> <?php echo $lang[$currentLang]['menu_api']; ?></a></li>
+      <div class="menu-container position-relative"> <!-- Bootstrap 5 používá position-relative místo style -->
+        <ul class="nav nav-tabs justify-content-center mb-3"> <!-- text-center je nahrazeno justify-content-center, margin-bottom: 10px je mb-3 -->
+          <li class="nav-item">
+            <a class="nav-link <?php if ($main_page == 'info.php') echo 'active'; ?>" href="info.php">
+              <span class="fa fa-info-circle"></span> <?php echo $lang[$currentLang]['menu_about']; ?>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if ($main_page == 'run.php') echo 'active'; ?>" href="run.php">
+              <span class="fa fa-cogs"></span> <?php echo $lang[$currentLang]['menu_run']; ?>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if ($main_page == 'api-reference.php') echo 'active'; ?>" href="api-reference.php">
+              <span class="fa fa-list"></span> <?php echo $lang[$currentLang]['menu_api']; ?>
+            </a>
+          </li>
           <!-- Přidání vlaječek pro změnu jazyka -->
           <?php
             if ($currentLang == 'cs') {
           ?>
-              <li style="right: 10px; position: absolute; margin-left: 10px;"><a href="?lang=en"><img src="img/flag_en.png" alt="English" style="height: 18px;"></a></li>
+              <li class="nav-item position-absolute" style="right: 10px;">
+                <a href="?lang=en" class="nav-link p-0"> <!-- p-0 odstraní padding, aby ikona nebyla příliš velká -->
+                  <img src="img/flag_en.png" alt="English" style="height: 18px;">
+                </a>
+              </li>
           <?php
             } else { 
           ?>
-              <li style="right: 10px; position: absolute; margin-left: 10px;"><a href="?lang=cs"><img src="img/flag_cs.png" alt="čeština" style="height: 18px;"></a></li>
+              <li class="nav-item position-absolute" style="right: 10px;">
+                <a href="?lang=cs" class="nav-link p-0">
+                  <img src="img/flag_cs.png" alt="čeština" style="height: 18px;">
+                </a>
+              </li>
           <?php
             }
           ?>
